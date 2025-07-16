6fde70db13bc691976771fd6926dc0a3753a65c9
Hamid.19471@gmail.com <hamid.19471@gmail.com>
2025-07-16 23:48:18 +0330

    Install jwt and cookie parser package and use cookie parser in main file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './configs/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.APP_PORT;
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   swaggerConfig(app);
+  const PORT = process.env.APP_PORT;
   await app.listen(PORT, () => {
     console.log(`server is running on port http://localhost:${PORT}`);
     console.log(`swagger is running on port http://localhost:${PORT}/swagger`);
