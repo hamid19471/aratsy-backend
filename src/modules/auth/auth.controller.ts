7392c087fc6e255ec1a6a9dfb3fff315a9601c8d
Hamid.19471@gmail.com <hamid.19471@gmail.com>
2025-07-18 15:52:39 +0330

    Update

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto/auth.dto';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
